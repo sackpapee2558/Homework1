e9c2506ab53ce0717a220a82d60f9d0e570dae4a
sackpapee2558 <77197348+sackpapee2558@users.noreply.github.com>
2021-02-11 14:47:29 +0700

    Add files via upload

---
+++ งาน1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\n";
+    cout << "1st Term of 2020          /6206021611052     Sackrapee Jantasuwan "<< endl;
+    cout << "\n"; 
+    cout << "----------------------------------------------------------------------------------"<< endl;
+    cout << "| Subject  ID  |         Subject name            | Score  (100)| Grade | "<< endl;
+    cout << "----------------------------------------------------------------------------------"<< endl;
+    cout << "|   4570034    | Introduction to computer        |     85      |   A   | "<< endl;
+    cout << "|   4580274    | English                         |     74      |   B   | "<< endl;
+    cout << "|   4530001    | Desingn Thinking                |     92      |   A   | "<< endl;
+    cout << "----------------------------------------------------------------------------------"<< endl;
+    cout << "|    TOTAL     | Subject = 3                     | Average = 3.87 | "<< endl;
+    cout << "================================================="<< endl;
+    cout << "\n" ;
+	cout << "\n" ;
+    cout << "KMUTNB"<< endl;
+return(0);
+}
